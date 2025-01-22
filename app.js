7a729e9b4176dce7069590d1d2b0573c57f5ec83
Alejandra Ordaz <aleog36@gmail.com>
2025-01-21 20:57:06 -0600

    Added: Function for save friends in a list

--- app.js
+++ app.js
@@ -1,1 +1,10 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let amigos = [];
+
+function agregarAmigo() {
+        let amigo = document.getElementById('amigo').value;
+        amigo == "" ? alert("Por favor, inserte un nombre.") : amigos.push(amigo);
+        console.log(amigos);
+}
+
+
