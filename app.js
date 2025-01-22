15bf82ad17f47a58b98625c0222cc6a8711e56d5
Alejandra Ordaz <aleog36@gmail.com>
2025-01-21 21:11:27 -0600

    Added: Function for clear input

--- app.js
+++ app.js
@@ -1,10 +1,20 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
 let amigos = [];
 
+//Función para agregar amigos
 function agregarAmigo() {
-        let amigo = document.getElementById('amigo').value;
-        amigo == "" ? alert("Por favor, inserte un nombre.") : amigos.push(amigo);
-        console.log(amigos);
+    //captura del valor del input
+    let amigo = document.getElementById('amigo').value;
+    // Condicional para validar o actualizar el array
+    amigo == ""
+    ? alert("Por favor, inserte un nombre.") 
+    : amigos.push(amigo);
+    limpiar();
+}
+
+// Funcion para limpiar el input
+function limpiar() {
+    document.getElementById('amigo').value = '';
 }
 
 
