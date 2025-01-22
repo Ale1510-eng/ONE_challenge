cb5d6bd4e4d089bbd2dcb44a733b795d23facdb7
Alejandra Ordaz <aleog36@gmail.com>
2025-01-21 22:27:23 -0600

    Added: Function for adding 'li' elements

--- app.js
+++ app.js
@@ -7,14 +7,29 @@ function agregarAmigo() {
     let amigo = document.getElementById('amigo').value;
     // Condicional para validar o actualizar el array
     amigo == ""
-    ? alert("Por favor, inserte un nombre.") 
-    : amigos.push(amigo);
+        ? alert("Por favor, inserte un nombre.")
+        : amigos.push(amigo);
     limpiar();
+    asignarTextoElemento('listaAmigos');
 }
 
 // Funcion para limpiar el input
 function limpiar() {
     document.getElementById('amigo').value = '';
 }
+
+function asignarTextoElemento(elemento) {
+    // Obtener elemento de la lista
+    let lista = document.getElementById(elemento);
+    // Limpiar lista existente
+    lista.innerHTML = "";
+    // iteración de los elementos de la lista de amigos
+    for (let i = 0; i < amigos.length; i++) {
+        // Añadir elementos a la lista
+        lista.innerHTML += `<li>${amigos[i]}</li>`;
+    }
+}
+
+
 
 
